6b8a9e03ee00d218089b9c81527219f3d47687b2
Chitin1627 <pratyushprashob@gmail.com>
2025-06-22 17:44:32 +0530

    Schemas learnt hehehe

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,16 +1,47 @@
 from fastapi import APIRouter
-from .schemas import EventSchema
+from .schemas import (
+    EventSchema, 
+    EventListSchema,
+    EventCreateSchema,
+    EventUpdateSchema
+)
 
 router = APIRouter()
 
+#List View
+# GET /api/events
 @router.get("/")
-def read_events():
+def read_events() -> EventListSchema:
     return {
-        "results": [1,2,3]
+        "results": [{"id": 1}, {"id": 2}, {"id": 3}],
+        "count": 3
     }
 
+
+#Create view
+# POST /api/events
+@router.post("/")
+def create_events(payload: EventCreateSchema) -> EventSchema:
+    print(payload)
+    data = payload.model_dump()
+    return {
+        "id": 123,
+        **data
+    }
+
+
+# GET /api/events/12
 @router.get("/{event_id}")
 def get_event(event_id: int) -> EventSchema:
     return {"id": event_id}
+
+
+#Update this data
+# PUT /api/events/12
+@router.put("/{event_id}")
+def update_event(event_id: int, payload: EventUpdateSchema) -> EventSchema:
+    print(payload)
+    data = payload.model_dump()
+    return {"id": event_id, **data}
 
 
